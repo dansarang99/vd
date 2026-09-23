988853b6530fb550c931aa828c8ad2190c46bdd3
dansarang99 <dansarang99@gmail.com>
2026-09-23 19:42:16 +0900

    fix: (AX)창업기술 이한규 대표 - vd28 주피터 노트북 [001] 단독 실행 세이프가드 보완

--- vd23_google_opal/command/02_Opal프롬프트생성기_실행.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-chcp 65001 > nul
-title [vd23] 02. Google Opal 프롬프트 생성기 CLI
-cd /d "%~dp0\..\src"
-python cli.py
-pause
--- vd23_google_opal/src/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-vd23 Google Opal App Prompt Builder Package
-"""
-__version__ = "1.0.0"
-__author__ = "BJ Jang (dansarang99)"
